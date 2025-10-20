8111f34ef28c38562da183b666bcbae18df338a5
raphael doehn <raphaeldoehn@automationgain.com>
2025-10-20 17:51:15 +0200

    add proper configuration for backend

--- infra/envs/test/1_networking.tf
+++ infra/envs/test/1_networking.tf
@@ -2,6 +2,11 @@
 # Network wiring, basically single-AZ - the 2nd private/public networks in different AZ's are only there because
 # ALB requires a 2nd public subnet (in different AZ) and RDS subnetgroup requires a 2nd private subnet (in different AZ)
 ########################################################################################################################
+# terraform import 'module.network.aws_vpc.this' vpc-0b8fd2ae364d56b05
+# terraform import 'module.network.aws_subnet.this["public-1"]'  subnet-0a3e9617937b3d3be
+# terraform import 'module.network.aws_subnet.this["public-2"]'  subnet-0d7db52ad299c0c03
+# terraform import 'module.network.aws_subnet.this["private-1"]' subnet-0324c2ca59cc19c80
+# terraform import 'module.network.aws_subnet.this["private-2"]' subnet-0bab14e7377a67d59
 
 module "network" {
   source = "../../modules/network"
