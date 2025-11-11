99790717e8adb250ca6954b01acfab8a8f998acb
raphael doehn <raphaeldoehn@automationgain.com>
2025-11-11 11:48:10 +0100

    import s3 resources

--- infra/envs/test/3_s3.tf
+++ infra/envs/test/3_s3.tf
@@ -1,3 +1,8 @@
+terraform import 'aws_s3_bucket.this' test-backend-inlocon
+terraform import 'aws_s3_bucket_ownership_controls.this' test-backend-inlocon
+terraform import 'aws_s3_bucket_public_access_block.this' test-backend-inlocon
+terraform import 'aws_s3_bucket_policy.this' test-backend-inlocon
+  
 module "s3" {
   source            = "../../modules/s3"
   bucket_name       = "test-backend-inlocon"
