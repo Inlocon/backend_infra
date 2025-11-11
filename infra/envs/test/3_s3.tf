2e6e191a3c3b20686104eaf327fd3b4197308861
raphael doehn <raphaeldoehn@automationgain.com>
2025-11-11 11:58:04 +0100

    different bucket name to simplify recreation

--- infra/envs/test/3_s3.tf
+++ infra/envs/test/3_s3.tf
@@ -1,11 +1,6 @@
-terraform import 'aws_s3_bucket.this' test-backend-inlocon
-terraform import 'aws_s3_bucket_ownership_controls.this' test-backend-inlocon
-terraform import 'aws_s3_bucket_public_access_block.this' test-backend-inlocon
-terraform import 'aws_s3_bucket_policy.this' test-backend-inlocon
-  
 module "s3" {
   source            = "../../modules/s3"
-  bucket_name       = "test-backend-inlocon"
+  bucket_name       = "test-backend-inlocon-1"
   # force_destroy     = true - default
   tags = { Env = "test" }
 }
