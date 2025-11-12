5bee37949742ab6c0d3b6f06ab28a325b61b30f4
raphael doehn <raphaeldoehn@automationgain.com>
2025-11-12 15:22:22 +0100

    remove user-data - connections with ssm works anyway

--- infra/modules/ec2_loader/main.tf
+++ infra/modules/ec2_loader/main.tf
@@ -30,31 +30,6 @@ resource "aws_instance" "this" {
   vpc_security_group_ids = [aws_security_group.this.id]
   iam_instance_profile = aws_iam_instance_profile.this.name
 
-    user_data = <<EOF
-#!/bin/bash
-set -euxo pipefail
-
-# Clean stale SSM registration & logs (when launching from an AMI)
-systemctl stop amazon-ssm-agent || true
-rm -rf /var/lib/amazon/ssm/* /var/log/amazon/ssm/* || true
-
-# Install/refresh SSM agent and start it
-if command -v yum >/dev/null 2>&1; then
-  yum -y install amazon-ssm-agent || yum -y reinstall amazon-ssm-agent
-  systemctl enable --now amazon-ssm-agent
-elif command -v apt-get >/dev/null 2>&1; then
-  # prefer deb if available; fall back to snap
-  apt-get update -y || true
-  apt-get install -y amazon-ssm-agent || true
-  systemctl enable --now amazon-ssm-agent || (
-    snap install amazon-ssm-agent --classic || true
-    systemctl enable --now snap.amazon-ssm-agent.amazon-ssm-agent.service
-  )
-fi
-EOF
-
-  user_data_replace_on_change = true
-
   metadata_options {
     http_tokens = "optional"
   }
