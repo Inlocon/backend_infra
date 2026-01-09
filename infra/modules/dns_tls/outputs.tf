f5e72fbfb42771f106c3369c709be36589543213
raphael doehn <raphaeldoehn@automationgain.com>
2026-01-09 15:15:18 +0100

    remove module dns_tls

--- infra/modules/dns_tls/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "certificate_arn" {
-  description = "Validated ACM certificate ARN"
-  value       = aws_acm_certificate.this.arn
-}
-
-output "record_fqdn" {
-  description = "Created Route53 record FQDN"
-  value       = aws_route53_record.app_alias.fqdn
-}
