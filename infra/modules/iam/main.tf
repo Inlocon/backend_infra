b8bdc6d0162d5e783dea70a0ef6e2a7eb9e53072
raphael doehn <raphaeldoehn@automationgain.com>
2026-01-15 18:14:32 +0100

    create task execution role and task role for dbsync container

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -1,11 +1,12 @@
 locals {
   services = {
     ec2 = "ec2.amazonaws.com"
-    webservice = "ecs-tasks.amazonaws.com"
+    ecsservice = "ecs-tasks.amazonaws.com"
   }
   tags = {
     ec2loader = { resourceGroup = "ec2-loader" }
     webservice = { resourceGroup = "webservice"}
+    dbsync = { resourceGroup = "dbsync"}
   }
   account_id = data.aws_caller_identity.current.account_id
 }
@@ -39,14 +40,26 @@ resource "aws_iam_role" "ec2_loader" {
 
 resource "aws_iam_role" "webservice_execution" {
   name               = "${var.env}-webservice-execution"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy["webservice"].json
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy["ecsservice"].json
   tags               = local.tags.webservice
 }
 
 resource "aws_iam_role" "webservice_task" {
   name               = "${var.env}-webservice-task"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy["webservice"].json
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy["ecsservice"].json
   tags               = local.tags.webservice
+}
+
+resource "aws_iam_role" "dbsync_execution" {
+  name               = "${var.env}-dbsync-execution"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy["ecsservice"].json
+  tags               = local.tags.dbsync
+}
+
+resource "aws_iam_role" "dbsync_task" {
+  name               = "${var.env}-dbsync-task"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy["ecsservice"].json
+  tags               = local.tags.dbsync
 }
 
 ######################
@@ -92,9 +105,9 @@ resource "aws_iam_role_policy_attachment" "ec2_loader" {
 }
 
 
-########################
-# webbservice execution
-########################
+###################################
+# webbservice and dbsync execution
+###################################
 
 data "aws_iam_policy_document" "webservice_execution" {
   statement {
@@ -119,6 +132,16 @@ resource "aws_iam_role_policy_attachment" "webservice_execution_custom_role" {
   policy_arn = aws_iam_policy.webservice_execution.arn
 }
 
+# just give the dbsync the same rights for now - will be deleted in a few month anway ...
+resource "aws_iam_role_policy_attachment" "dbsync_execution_managed_task_execution_role" {
+  role       = aws_iam_role.dbsync_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "dbsync_execution_custom_role" {
+  role       = aws_iam_role.dbsync_execution.name
+  policy_arn = aws_iam_policy.webservice_execution.arn
+}
 ##################
 # webservice task
 ##################
@@ -157,6 +180,12 @@ resource "aws_iam_policy" "webservice_task" {
 
 resource "aws_iam_role_policy_attachment" "webservice_task" {
   role = aws_iam_role.webservice_task.name
+  policy_arn = aws_iam_policy.webservice_task.arn
+}
+
+# just give the dbsync the same rights for now - will be deleted in a few month anway ...
+resource "aws_iam_role_policy_attachment" "dbsync_task" {
+  role = aws_iam_role.dbsync_task.name
   policy_arn = aws_iam_policy.webservice_task.arn
 }
 
--- infra/modules/iam/outputs.tf
+++ infra/modules/iam/outputs.tf
@@ -7,4 +7,9 @@ output "webservice_execution_role_name" { value = aws_iam_role.webservice_execut
 output "webservice_task_role_arn" { value = aws_iam_role.webservice_task.arn }
 output "webservice_task_role_name" { value = aws_iam_role.webservice_task.name }
 
+output "dbsync_execution_role_arn" { value = aws_iam_role.dbsync_execution.arn }
+output "dbsync_execution_role_name" { value = aws_iam_role.dbsync_execution.name }
+
+output "dbsync_task_role_arn" { value = aws_iam_role.dbsync_task.arn }
+output "dbsync_task_role_name" { value = aws_iam_role.dbsync_task.name }
 
