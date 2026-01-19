64a90add630a6a0f22eaee458c358836db901aa9
raphael doehn <raphaeldoehn@automationgain.com>
2026-01-19 13:32:36 +0100

    revert deployment_maximum_percent and deployment_minimum_healthy_percent to default values

--- infra/modules/8_dbsync/1_ecs_service.tf
+++ infra/modules/8_dbsync/1_ecs_service.tf
@@ -4,8 +4,6 @@ resource "aws_ecs_service" "this" {
   launch_type     = "FARGATE"
   platform_version = "LATEST"
   desired_count = 1
-  deployment_maximum_percent         = 100
-  deployment_minimum_healthy_percent = 0
 
 
   network_configuration {
