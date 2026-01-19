40a7377cca55701f448c1db40dcaaf01da1eaba9
raphael doehn <raphaeldoehn@automationgain.com>
2026-01-19 14:35:47 +0100

    add IAM to prod

--- infra/envs/prod/0_iam.tf
+++ infra/envs/prod/0_iam.tf
@@ -1,6 +1,6 @@
-# module "iam" {
-#   source                     = "../../modules/0_iam"
-#   env                        = "test"
-#   secret_name_db_credentials = local.secret_name_db_credentials
-#   bucket_name                = "${var.env}-backend-inlocon"
-# }
+module "iam" {
+  source                     = "../../modules/0_iam"
+  env                        = var.env
+  secret_name_db_credentials = local.secret_name_db_credentials
+  bucket_name                = "${var.env}-backend-inlocon"
+}
