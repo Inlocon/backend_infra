64194164c0cc7fac837382234fb6fd29d41cd6d6
raphael doehn <raphaeldoehn@automationgain.com>
2026-01-29 11:51:52 +0100

    add s3 bucket for files

--- infra/envs/test/3_s3.tf
+++ infra/envs/test/3_s3.tf
@@ -3,3 +3,9 @@ module "s3" {
   source      = "../../modules/3_s3"
   bucket_name = "${var.env}-backend-inlocon"
 }
+
+module "s3_files" {
+  env = var.env
+  source      = "../../modules/3_s3"
+  bucket_name = "${var.env}-files-inlocon"
+}
