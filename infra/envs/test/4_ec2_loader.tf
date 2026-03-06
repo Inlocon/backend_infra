8d8552d042d9965f066adba81844eda6d71a2b29
raphael doehn <raphaeldoehn@automationgain.com>
2026-03-06 17:14:52 +0100

    update tf version; move from t2.micro to t3.medium to get long_texts lv done

--- infra/envs/test/4_ec2_loader.tf
+++ infra/envs/test/4_ec2_loader.tf
@@ -3,7 +3,7 @@ module "ec2_loader" {
   ami_id        = "ami-09361b64d009bc090"
   env           = var.env
   iam_role_name = module.iam.ec2_loader_role_name # "ec2-migration"
-  instance_type = "t2.micro"
+  instance_type = "t3.medium"
   name          = "ec2-db-connector"
   # put in public subnet
   # a) much simpler (and cheaper) - avoids creation of unnecessary vpc endpoints and/or NAT gateway in the private subnet(s)
